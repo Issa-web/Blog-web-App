6cb5aacef6e09464bbc85ec459a2d2b05c3dc67d
Issa sangare <issabmsangare@hotmail.com>
2020-12-16 01:13:53 -0500

    start creating database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
 const _ = require("lodash");
 
@@ -9,7 +10,7 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-let posts = [];
+let blogPosts = [];
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -17,8 +18,29 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+// connecting blog to my databse
+mongoose.connect('mongodb://localhost:27017/blogsDB', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+// creating shema
+const blogsSchema = {
+  title : String,
+  body: String
+}
+// creating model
+const Blog = mongoose.model("Blog", blogsSchema);
+
+const blog1 = new Blog({
+    title: "Day1",
+    body: "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing."
+});
+const blog2 = new Blog({
+  title: "Day2",
+  body: "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui."
+});
+//blog1.save();
+// blog2.save();
+const initialBlogs = [blog1, blog2]
 app.get("/", (req, res) =>{
-    res.render("home", {homeStartingContent: homeStartingContent, posts: posts})
+    res.render("home", {homeStartingContent: homeStartingContent, initialBlogs: initialBlogs})
 });
 
 app.get("/about", (req, res) =>{
@@ -34,28 +56,30 @@ app.get("/compose", (req, res) =>{
 });
 
 app.post("/compose", (req, res) =>{
-  let post = {
-    title: req.body.postTitle,
-    content: req.body.postBody
-  }
-  posts.push(post);
+  // console.log(req.body)
+  let newBlog = new Blog({
+      title: req.body.postTitle,
+      body: req.body.postBody
+  })
+  // newBlog.save()
+  initialBlogs.push(newBlog);
   res.redirect("/")
 });
 
-app.get("/posts/:postName", (req, res) =>{
-  let requestTitle = _.lowerCase(req.params.postName);
-  posts.forEach((post) =>{
-    let storeTitle = _.lowerCase(post.title)
-   if(storeTitle === requestTitle){
-     res.render("post",
-      {
-        title: post.title,
-        content: post.content
-      })
-   }
-  })
+// app.get("/posts/:postName", (req, res) =>{
+//   let requestTitle = _.lowerCase(req.params.postName);
+//   posts.forEach((post) =>{
+//     let storeTitle = _.lowerCase(post.title)
+//    if(storeTitle === requestTitle){
+//      res.render("post",
+//       {
+//         title: post.title,
+//         content: post.content
+//       })
+//    }
+//   })
   
-})
+// })
 
 
 
